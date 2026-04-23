d1cc22fac71c6b1cf6ff5b356b0c7aac1307cf07
Benjamin Heinen <heinen.benjamin@gmail.com>
2026-04-23 19:45:53 -0400

    feat: configure GitHub Actions deployment and static export for Next.js project

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = '';
+let basePath = '';
+
+if (isGithubActions) {
+  // Le nom du dépôt est 'architectures'
+  const repo = 'architectures';
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  images: {
+    unoptimized: true, // Requis pour l'export statique GitHub Pages si on utilisait next/image
+  },
 };
 
 export default nextConfig;
